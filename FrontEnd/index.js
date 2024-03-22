e2a68cc542c3b2f1040584b7a786f30bad18775d
Bishnu Mishra <mishravishnu369@gmail.com>
2024-03-22 13:22:43 +0530

    icon animation on dragging file

---
+++ FrontEnd/index.js
@@ -0,0 +1,18 @@
+const dropZone = document.querySelector(".drop-zone");
+
+dropZone.addEventListener("dragover", (e) => {
+    e.preventDefault();
+
+    if(!dropZone.classList.contains("dragged")) {
+        dropZone.classList.add("dragged");
+    } 
+});
+
+dropZone.addEventListener("dragleave", () => {
+    dropZone.classList.remove("dragged");
+});
+
+dropZone.addEventListener("drop", (e) => {
+    e.preventDefault();
+    dropZone.classList.remove("dragged");
+});
