28efdee05b1ecfb49db13c85b1979990b0c8a33a
Bishnu Mishra <mishravishnu369@gmail.com>
2024-04-01 21:28:51 +0530

    Removed extra '/' from "/files" in the line " downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}?source=email` " in Mail sending functionality of files.js in router

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ connectDB();
 async function fetchData() {
     const past24hrDate = new Date(Date.now() - 24 * 60 * 60 * 1000); // Date.now() fetches current timestamp in milliseconds
     const files = File.find({ createdAt: { $lt: past24hrDate } });
-    console.log("File length", files.length);
+    console.log("File length:", files.length);
 
     if (files.length) {
         for (const file of files) {
