cd041f48186d3e5b9e59233ed5ebd58d296da182
Bishnu Mishra <mishravishnu369@gmail.com>
2024-04-04 16:11:48 +0530

    fixed some codes in script.js

--- script.js
+++ script.js
@@ -8,17 +8,16 @@ connectDB();
 
 // Delete all files older than 24 hours
 async function fetchData() {
-    const past24hrDate = new Date(Date.now() - 24 * 60 * 60 * 1000); // Date.now() fetches current timestamp in milliseconds
-    const files = File.find({ createdAt: { $lt: past24hrDate } });
+    const past24hrDate = new Date(Date.now() - 24 * 60 * 60 * 1000);   // Date.now() fetches current timestamp in milliseconds
+    const files = await File.find({ createdAt: { $lt: past24hrDate } });
     console.log("File length:", files.length);
 
     if (files.length) {
         for (const file of files) {
             try {
-                fs.unlinkSync(file.path);  // unlinkSync() deletes the file synchronously
-                await file.remove();  // file gets deleted from database
+                await file.deleteOne();
                 console.log(`Successfully deleted ${file.filename}`);
-            } catch(err) {
+            } catch (err) {
                 console.log(`Error while deleting file: ${err}`);
             }
         }
