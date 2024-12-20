83df19ea3a028306e5cdca6ba1a620a403646eee
Bishnu Mishra <mishravishnu369@gmail.com>
2024-12-20 17:39:13 +0530

    added a / in download link of email

--- routes/files.js
+++ routes/files.js
@@ -71,7 +71,7 @@ router.post('/send', async (req, res) => {
             text: `${emailFrom} shared a file with you.`,
             html: require('../services/emailTemplate')({
                     emailFrom, 
-                    downloadLink: `${process.env.APP_BASE_URL}files/${file.uuid}?source=email` ,
+                    downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}?source=email` ,
                     size: parseInt(file.size/1024) + ' KB',
                     expires: '24 hours'
                 })
